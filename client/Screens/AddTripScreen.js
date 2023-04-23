ed6d31301da2116c2ac2d1c96dc6627564397473
the1mag <the1mag@g.ucla.edu>
2023-04-22 20:07:13 -0700

    add trip screen

--- client/Screens/AddTripScreen.js
+++ client/Screens/AddTripScreen.js
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   Text,
   Modal,
+  Animated,
+  TextInput,
 } from "react-native";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
@@ -19,19 +21,50 @@ const AddTripScreen = ({ navigation }) => {
   function handleClose() {
     setShowModal(false);
   }
+  const [pressedButton, setPressedButton] = useState("");
 
-  const [pressedButton, setPressedButton] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleAnimation = new Animated.Value(isOpen ? 1 : 0);
+
+  // const translateY = new Animated.Value(0);
+  const [price, setPrice] = useState("");
+  const [pass, setPass] = useState("");
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    Animated.timing(toggleAnimation, {
+      toValue: isOpen ? 0 : 1,
+      duration: 300,
+      useNativeDriver: false
+    }).start();
+  }
+
+  // const closeMenu = () => {
+  //   console.log("poop")
+  //   Animated.timing(translateY, {
+  //     toValue: 0,
+  //     duration: 500,
+  //     useNativeDriver: true,
+  //   }).start(() => setIsOpen(false));
+  // };
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={{ width: "90%", position: "absolute", marginVertical:'10%', marginHorizontal:'5%' }}>
+      <TouchableOpacity
+        style={{
+          width: "90%",
+          position: "absolute",
+          marginVertical: "10%",
+          marginHorizontal: "5%",
+        }}
+      >
         <MaterialIcons
           name="arrow-back-ios"
           size={30}
           color={"#000000"}
           onPress={() => navigation.goBack()}
         />
-      </TouchableOpacity>        
+      </TouchableOpacity>
       <Text style={styles.title}>Add Trip</Text>
       <View style={styles.textCont}>
         <Text style={styles.header}>Trip Details </Text>
@@ -44,14 +77,65 @@ const AddTripScreen = ({ navigation }) => {
         <Text style={styles.box}>10:15 am 4/31/23 </Text>
         <Text style={styles.header}>Join As</Text>
         <Text style={styles.text}>Carpool</Text>
-        <TouchableOpacity style={[styles.carpoolbutton, pressedButton ==='button1' ? styles.pressed :null,]}         onPress={() => setPressedButton('button1')}
->
-          <Text style={styles.buttonText}>Driver</Text>
+        {/* BUTTON */}
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={[
+              styles.carpoolbutton,
+              pressedButton === "button1" ? styles.pressed : null,
+            ]}
+            onPress={() => {
+              setPressedButton("button1");
+              {
+                toggleMenu;
+              }
+            }}
+          >
+            <Text style={styles.buttonText}>{isOpen ? 'Close' : 'Open'}Driver</Text>
+          </TouchableOpacity>
+          <Animated.View style={[styles.popup, {transform: [{translateY: toggleAnimation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [5, 0]
+      })}]}]}>
+                <Text style={styles.textCont}>This is the toggle bar</Text>
+                <Text style={styles.header}>Driver Details</Text>
+          <Text style={styles.textCont}>Set Price</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="ex: 10.00"
+            value={price}
+            onChangeText={(text) => setPrice(text)}
+          />
+          <Text style={styles.textCont}>Num. Passengers</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="ex: 3"
+            value={pass}
+            onChangeText={(text) => setPass(text)}
+          />
+        <TouchableOpacity style={styles.button} onPress={toggleMenu}>
+          <Text>OK</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.carpoolbutton, pressedButton==='button2' ? styles.pressed : null,]} onPress={() => setPressedButton('button2')}>
+      </Animated.View>
+        </View>
+        <TouchableOpacity
+          style={[
+            styles.carpoolbutton,
+            pressedButton === "button2" ? styles.pressed : null,
+          ]}
+          onPress={() => setPressedButton("button2")}
+        >
           <Text style={styles.buttonText}>Passenger</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, pressedButton==='button3' ? styles.pressed : null,]}onPress={() => setPressedButton('button3')}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            pressedButton === "button3" ? styles.pressed : null,
+          ]}
+          onPress={() => setPressedButton("button3")}
+        >
           <Text style={styles.buttonText}>Start Uber group</Text>
         </TouchableOpacity>
 
@@ -81,6 +165,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#E5E7F6",
   },
+  popup: {
+    backgroundColor: "#8C96F0",
+  },
   textCont: {
     textAlign: "left",
     marginHorizontal: "10%",
@@ -91,10 +178,10 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
   },
-  header:{
+  header: {
     textAlign: "center",
     fontSize: 20,
-    fontWeight: "bold", 
+    fontWeight: "bold",
   },
   box: {
     height: 40,
@@ -118,7 +205,7 @@ const styles = StyleSheet.create({
     marginBottom: 28,
     marginTop: 40,
   },
-  pressed:{
+  pressed: {
     backgroundColor: "#8C96F0",
   },
   addbutton: {
@@ -157,6 +244,27 @@ const styles = StyleSheet.create({
   modalCloseButton: {
     color: "blue",
     textDecorationLine: "underline",
+  },
+  menu: {
+    marginHorizontal: "0%",
+    marginVertical: "0%",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#BBC0F4",
+    padding: 30,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    zIndex: 3,
+  },
+  input: {
+    borderWidth: 1,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    marginTop:10,
   },
 });
 
